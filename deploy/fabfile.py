093e227098370b752024e1467cde4501016c1af8
Marcos Daniel Petry <marcospetry@gmail.com>
2011-11-19 20:04:35 -0200

    added gunicorn and django scripts

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -18,6 +18,10 @@ env.server_project_dir = '/srv/%s/' % env.project_name
 env.server_home_dir = '/home/%s/' % env.server_user
 env.enviroment_dir = '%senviroment/' % env.server_home_dir
 
+env.gunicorn_port = '8081'
+
+
+
 def nginx_server_base_configuration():
     puts('adding HTTP Server config files for project')
     files.upload_template("%s/deploy/nginx/server.conf" % env.project_dir,
@@ -25,12 +29,16 @@ def nginx_server_base_configuration():
                           use_sudo=True)
     restart_service('nginx')
 
+
+
 def nginx_setup():
     puts('installing NGINX')
     install_packages([
         'nginx',
     ])
     nginx_server_base_configuration()
+
+
 
 def nginx_server_project_configuration():
     puts('adding HTTP Server config files')
@@ -44,12 +52,74 @@ def nginx_server_project_configuration():
                           use_sudo=True)
     restart_service('nginx')
 
+
+
+def gunicorn_setup():
+    puts('Add Gunicorn init script')
+    files.upload_template("%s/deploy/gunicorn/gunicorn_django_server" % env.project_dir,
+                          "/etc/init.d/gunicorn_django",
+                          use_sudo=True)
+    sudo('chmod +x /etc/init.d/gunicorn_django')
+
+
+def install_pip_dependancies():
+    puts('install pip dependaecies')
+    requirements = open('%s/requirements.txt' % env.project_dir, 'r')
+    for module in requirements.readlines():
+        run('%sbin/pip install %s' % (env.enviroment_dir, module))
+
+
+def upload_project():
+    puts('uploading project files')
+    local('cd %(project_dir)s && tar -zcvf /tmp/%(project_name)s.tar %(project_name)s' % env)
+    files.upload_template("/tmp/%s.tar" % env.project_name,
+                          "%(server_project_dir)s%(project_name)s.tar" % env)
+    run('tar -zxvf %(server_project_dir)s%(project_name)s.tar -C %(server_project_dir)s' % env)
+
+
+def python_install_virtualenv():
+    install_packages([
+        'python-virtualenv',
+        ])
+    create_directories(env.server_project_dir, env.server_user, '0750')
+    if not files.exists("%sbin/activate" % env.enviroment_dir):
+        run("virtualenv --distribute --no-site-packages %s" % env.enviroment_dir)
+
+
+def python_enviroment():
+    puts('base python libs for system')
+    python_install_virtualenv()
+
+    install_pip_dependancies()
+
+
+
+def gunicorn_deploy():
+    puts('transferring gunicorn configurarion and restart')
+    files.upload_template("%s/deploy/gunicorn/gunicorn_django_site" % env.project_dir,
+                          "/etc/default/gunicorn_django-%s" % env.project_name,
+                          context=env,
+                          use_sudo=True)
+
+    sudo('/etc/init.d/gunicorn_django restart %s' % env.project_name, pty=False)
+
+
 def deploy():
     puts("Deploying Project...")
     nginx_server_project_configuration()
 
+    python_enviroment()
+
+    upload_project()
+
+    gunicorn_deploy()
+
+
+
+
 def setup():
     server_upgrade()
     nginx_setup()
+    gunicorn_setup()
     deploy()
 
--- deploy/fabutils.py
+++ deploy/fabutils.py
@@ -37,5 +37,32 @@ def restart_service(service):
     sudo("service %s stop" % (service,))
     sudo("service %s start" % (service,))
 
+def new_user(admin_username, admin_password, group):
+    # Create the admin group and add it to the sudoers file
+    admin_group = group
+    try:
+        sudo('addgroup {group}'.format(group=admin_group))
+        sudo('echo "%{group} ALL=(ALL) ALL" >> /etc/sudoers'.format(group=admin_group))
+    except:
+        pass
 
+    # Create the new admin user (default group=username); add to admin group
+    sudo('adduser {username} --disabled-password --gecos ""'.format(
+        username=admin_username))
+    sudo('adduser {username} {group}'.format(
+        username=admin_username,
+        group=admin_group))
+
+    # Set the password for the new admin user
+    sudo('echo "{username}:{password}" | chpasswd'.format(
+        username=admin_username,
+        password=admin_password))
+
+
+def create_directories(path, user, permission, group=None):
+    group = group or user
+
+    sudo("mkdir -p %s" % path)
+    sudo("chown %s:%s %s" % (user, group, path))
+    sudo("chmod %s %s" % (permission, path))
     
