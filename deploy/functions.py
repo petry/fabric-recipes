933d9b501898ff2cd3ab1513c86affcecf267a87
Marcos Daniel Petry <marcospetry@gmail.com>
2012-12-15 11:51:01 -0200

    put deploy and setup unfcion on proeject

--- deploy/functions.py
+++ deploy/functions.py
@@ -1,21 +1,7 @@
-import os
 from fabric.contrib import files
-from fabric.decorators import task
 from fabric.operations import run
 from fabric.api import env, local, sudo
-from fabutils import install_packages, restart_service, server_upgrade, create_directories, puts
-
-env.project_name = "project"
-env.project_dir = os.path.realpath(os.path.join(os.path.dirname(env.real_fabfile), ".."))
-
-env.user = "usr_%s" % env.project_name
-env.password = env.user
-env.host_string = '127.0.0.1:2222'
-
-env.server_user = env.user
-env.server_project_dir = '/srv/%s/' % env.project_name
-env.server_home_dir = '/home/%s/' % env.server_user
-env.enviroment_dir = '%senviroment/' % env.server_home_dir
+from fabutils import install_packages, restart_service, create_directories, puts
 
 
 def nginx_server_base_configuration(project_dir):
@@ -105,23 +91,3 @@ def gunicorn_deploy(project_dir, project_name):
                           use_sudo=True)
 
     sudo('/etc/init.d/gunicorn_django restart %s' % project_name, pty=False)
-
-
-@task
-def deploy():
-    puts("Deploying Project...")
-    nginx_server_project_configuration(
-        project_dir=env.project_dir,
-        project_name=env.project_name)
-
-    python_enviroment(project_dir=env.project_dir)
-    upload_project(project_name=env.project_name)
-    gunicorn_deploy(project_dir=env.project_dir, project_name=env.project_name)
-
-
-@task
-def setup():
-    server_upgrade()
-    nginx_setup(project_dir=env.project_dir)
-    gunicorn_setup(project_dir=env.project_dir)
-    deploy()
